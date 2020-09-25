a7dd488d03992bf57ae2d6705c1fff94083434ca
nithin <nithin1537@gmail.com>
2020-09-25 23:12:42 +0530

    first commit in automation branch

--- src/test/java/com/mertisoft/fxo/myclass.java
+++ src/test/java/com/mertisoft/fxo/myclass.java
@@ -7,7 +7,7 @@ public class myclass {
     public static void main(String[]args){
         
         System.out.println(name);
-
+        System.out.println(name);System.out.println(name);System.out.println(name);System.out.println(name);System.out.println(name);System.out.println(name);
     }
 
 
