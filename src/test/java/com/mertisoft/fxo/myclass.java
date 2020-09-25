956c16893fcc4ee8dcec5fabeb5b8af3dbd935be
nithin <nithin1537@gmail.com>
2020-09-25 20:59:26 +0530

    this is second commit program

---
+++ src/test/java/com/mertisoft/fxo/myclass.java
@@ -0,0 +1,14 @@
+package com.mertisoft.fxo;
+
+public class myclass {
+
+     private static String name="nithin";
+
+    public static void main(String[]args){
+        
+        System.out.println(name);
+
+    }
+
+
+}
